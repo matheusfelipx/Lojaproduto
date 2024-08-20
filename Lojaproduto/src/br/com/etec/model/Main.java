f88f2fbcc67dacf65e7d46d06d7a2f51f52a9f9b
lab04 <lab04@lab4-19>
2024-08-19 22:46:51 -0300

    Criado tela de login, colocando funcionalidade no botao e nos campos de texto

--- Lojaproduto/src/br/com/etec/model/Main.java
+++ Lojaproduto/src/br/com/etec/model/Main.java
@@ -12,9 +12,9 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("/br/com/etec/view/telaLogin.fxml"));
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("/br/com/etec/view/application.css").toExternalForm());
-			primaryStage.setScene(scene);
+			primaryStage.setScene(scene);			
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
